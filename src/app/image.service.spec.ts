cd5150b70a73062e1fd34083106411f908c9e232
dileyson <dem4.echeverria@gmail.com>
2022-09-04 20:53:11 -0500

    test added

--- src/app/image.service.spec.ts
+++ src/app/image.service.spec.ts
@@ -6,11 +6,34 @@ describe('ImageService', () => {
   let service: ImageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ImageService]
+    });
     service = TestBed.inject(ImageService);
   });
 
-  it('should be created', () => {
+  it('Debe crear el servicio', () => {
     expect(service).toBeTruthy();
   });
+
+  
+  it('Mostrar todas las imagenes', () => {
+    let allImages = service.getImages()
+    expect(allImages.length).toEqual(5)
+
+  })
+
+  it('Retornar una imagen dada (en este caso la imagen de id=1)', () => {
+    let image = service.getImage(1)
+    expect(image.id).toEqual(1)
+
+  })
+
+  it('Retornar indefinido al pasar un id que no existe(en este caso la imagen de id=10)', () => {
+    let image = service.getImage(10)
+    expect(image).toBeUndefined()
+  })
+
+
+
 });
